08c59b3cd8a3f63b199fe82e823c50c81400368d
Bohdan Bodak <noopps0@gmail.com>
2018-06-22 02:54:39 +0300

    Added new views, tables and view controllers for mobile app

---
+++ ClientApp.iOS/mmos/ViewControllers/CreateChannelViewController.swift
@@ -0,0 +1,13 @@
+//
+//  CreateChannelViewController.swift
+//  mmos
+//
+//  Created by 8teRnity on 6/21/18.
+//  Copyright © 2018 kpi. All rights reserved.
+//
+
+import UIKit
+
+class CreateChannelViewController : UIViewController {
+    
+}
---
+++ ClientApp.iOS/mmos/ViewControllers/CreateTeamViewController.swift
@@ -0,0 +1,13 @@
+//
+//  CreateTeamViewController.swift
+//  mmos
+//
+//  Created by 8teRnity on 6/22/18.
+//  Copyright © 2018 kpi. All rights reserved.
+//
+
+import UIKit
+
+class CreateTeamViewController : UIViewController {
+    
+}
